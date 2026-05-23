fca6d5b125396b177e54e70d8e96742e7b130fc7
Alan Canon <aronnax9000@gmail.com>
2026-05-23 13:15:56 -0400

    Update checkproof.js

--- checkproof/checkproof.js
+++ checkproof/checkproof.js
@@ -8,14 +8,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 var truths = []
 
 function seed_truths() {
-  truths["0"] = "Rule 0"]
+  truths["0"] = "Rule 0"
 }
 
 function display_truths() {
   var truth_element = getElementById("truths")
   truth_element.innerHtml = ""
   truths.forEach(value, index => {
-  truth_element.innerhtml += index + " " + value + "\n"
-})
-  
+    truth_element.innerhtml += index + " " + value + "\n"
+  })
 }
