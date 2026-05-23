0c2d211497e52bd235a1d4a035ef4f8752d41750
Alan Canon <aronnax9000@gmail.com>
2026-05-23 13:08:58 -0400

    Update checkproof.js

--- checkproof/checkproof.js
+++ checkproof/checkproof.js
@@ -1,5 +1,21 @@
 // Checkproof
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-  alert("Document loaded")
+  seed_truths()
 });
+
+
+var truths = []
+
+function seed_truths() {
+  truths["0"] = "Rule 0"]
+}
+
+function display_truths() {
+  var truth_element = getElementById("truths")
+  truth_element.innerHtml = ""
+  truths.forEach(value => {
+  truth_element.innerhtml += 
+})
+  
+}
