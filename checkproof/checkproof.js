40653564ba1ccb59d13c71460e2103c15cf83acd
Alan Canon <aronnax9000@gmail.com>
2026-05-23 13:30:24 -0400

    Update checkproof.js

--- checkproof/checkproof.js
+++ checkproof/checkproof.js
@@ -15,7 +15,7 @@ function seed_truths() {
 function display_truths() {
   var truth_element = document.getElementById("truths")
   truth_element.innerHtml = ""
-  truths.forEach(value, index => {
+  truths.forEach(value, index, array => {
     text_to_add = index + " " + value + "\n"
     alert(text_to_add)
     truth_element.innerhtml += text_to_add
